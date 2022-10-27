2b82afa8244f9f9a775859e4ac962688702de3c2
Viktor Tovarnykh <milaslavska2@gmail.com>
2022-10-27 15:38:04 +0200

    Anagrams.Class Refactor

--- web-application/src/main/java/ua/com/foxminded/springcourse/cleancode/task_one/Anagrams.java
+++ web-application/src/main/java/ua/com/foxminded/springcourse/cleancode/task_one/Anagrams.java
@@ -1,10 +1,21 @@
 package ua.com.foxminded.springcourse.cleancode.task_one;
 
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Anagrams {
 
+	/**
+	 * Function name: reverseWord
+	 * 
+	 * @return (String)
+	 * @throws NullPointerException, IllegalArgumentException
+	 * 
+	 * Inside the function: 1. Identifying left and right borders of one word;
+	 * 2. Looking for the signs on the both sides, if the
+	 * conditions are accepted, then it changes the places;
+	 */
 	public static String reverseWord(String word){
 
 		if (word == null)
@@ -18,16 +29,16 @@ public class Anagrams {
 
 		while (leftWordBorder < rightWordBorder) {
 
-			if (!Character.isAlphabetic(reversedWord.charAt(leftWordBorder)))
+			if (!Character.isAlphabetic( reversedWord.charAt( leftWordBorder ) ))
 				leftWordBorder++;
 
-			else if (!Character.isAlphabetic(reversedWord.charAt(rightWordBorder)))
+			else if (!Character.isAlphabetic( reversedWord.charAt( rightWordBorder ) ))
 				rightWordBorder--;
 
 			else {
-				char tmp = reversedWord.charAt(leftWordBorder);
-				reversedWord.setCharAt(leftWordBorder, reversedWord.charAt(rightWordBorder));
-				reversedWord.setCharAt(rightWordBorder, tmp);
+				char tmp = reversedWord.charAt( leftWordBorder );
+				reversedWord.setCharAt( leftWordBorder, reversedWord.charAt( rightWordBorder ) );
+				reversedWord.setCharAt( rightWordBorder, tmp );
 				leftWordBorder++;
 				rightWordBorder--;
 			}
@@ -36,7 +47,17 @@ public class Anagrams {
 
 		return reversedWord.toString();
 	}
-
+	
+	/**
+	 * Function name: reverseSentence
+	 * 
+	 * @return (String)
+	 * @throws NullPointerException, IllegalArgumentException
+	 * 
+	 * Inside the function: 1. Splitting String on words;
+	 * 2. Calling the method reverseWord() for each word;
+	 * 3. Joining back words in one sentence
+	 */
 	public static String reverseSentence(String sentence) {
 		
 		if (sentence == null)
@@ -45,7 +66,7 @@ public class Anagrams {
 		if (sentence.isBlank())
 			throw new IllegalArgumentException("String is blank");
 
-		return Arrays.stream(sentence.split(" ")).map(word -> word = reverseWord(word))
+		return Arrays.stream(sentence.split(" ")).map( word -> word = reverseWord( word ) )
 				.collect(Collectors.joining(" "));
 		
 	}
