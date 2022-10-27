22e922d874b86f163124ab98a431b9e85bb3e53f
Viktor Tovarnykh <milaslavska2@gmail.com>
2022-10-27 15:22:47 +0200

    Implemented sentence reverse feature

--- web-application/src/main/java/ua/com/foxminded/springcourse/cleancode/task_one/Anagrams.java
+++ web-application/src/main/java/ua/com/foxminded/springcourse/cleancode/task_one/Anagrams.java
@@ -1,14 +1,18 @@
 package ua.com.foxminded.springcourse.cleancode.task_one;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Anagrams {
 
-	public static String reverseWord(String word) {
-		
-		if(word == null)
+	public static String reverseWord(String word){
+
+		if (word == null)
 			throw new NullPointerException("Argument can not be null");
-		if(word.isBlank())
-			throw new IllegalArgumentException("String is blank");
 		
+		if (word.isBlank())
+			throw new IllegalArgumentException("String is blank");
+
 		StringBuilder reversedWord = new StringBuilder(word);
 		int rightWordBorder = word.length() - 1, leftWordBorder = 0;
 
@@ -16,10 +20,10 @@ public class Anagrams {
 
 			if (!Character.isAlphabetic(reversedWord.charAt(leftWordBorder)))
 				leftWordBorder++;
-			
+
 			else if (!Character.isAlphabetic(reversedWord.charAt(rightWordBorder)))
 				rightWordBorder--;
-			
+
 			else {
 				char tmp = reversedWord.charAt(leftWordBorder);
 				reversedWord.setCharAt(leftWordBorder, reversedWord.charAt(rightWordBorder));
@@ -27,10 +31,23 @@ public class Anagrams {
 				leftWordBorder++;
 				rightWordBorder--;
 			}
-			
+
 		}
 
 		return reversedWord.toString();
+	}
+
+	public static String reverseSentence(String sentence) {
+		
+		if (sentence == null)
+			throw new NullPointerException("Argument can not be null");
+		
+		if (sentence.isBlank())
+			throw new IllegalArgumentException("String is blank");
+
+		return Arrays.stream(sentence.split(" ")).map(word -> word = reverseWord(word))
+				.collect(Collectors.joining(" "));
+		
 	}
 
 }
