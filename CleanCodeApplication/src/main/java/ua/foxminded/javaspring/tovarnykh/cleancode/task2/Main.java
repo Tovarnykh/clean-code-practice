cdd6feb7b3e323ccaccaed86e2c0b84b6f2faca7
Viktor Tovarnykh <milaslavska2@gmail.com>
2022-11-04 16:29:04 +0100

    IntegerDivision is done. Tests have written

---
+++ CleanCodeApplication/src/main/java/ua/foxminded/javaspring/tovarnykh/cleancode/task2/Main.java
@@ -0,0 +1,10 @@
+package ua.foxminded.javaspring.tovarnykh.cleancode.task2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(IntegerDivision.makeDivision(333, 2));
+		
+	}
+
+}
---
+++ CleanCodeApplication/src/test/java/ua/foxminded/javaspring/tovarnykh/cleancode/task2/IntegerDivisionTest.java
@@ -0,0 +1,115 @@
+package ua.foxminded.javaspring.tovarnykh.cleancode.task2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import ua.foxminded.javaspring.tovarnykh.cleancode.task1.Anagrams;
+
+class IntegerDivisionTest {
+
+	@Test
+   void makeDivision_CommonDivision_True() {
+       String expected =   "_78945|4\n" + 
+                           " 4    |-----\n"+
+                           " -    |19736\n" + 
+                           "_38\n" + 
+                           " 36\n" + 
+                           " --\n"+
+                           " _29\n"+
+                           "  28\n" + 
+                           "  --\n" + 
+                           "  _14\n" + 
+                           "   12\n" + 
+                           "   --\n"+
+                           "   _25\n"+
+                           "    24\n"+
+                           "    --\n"+
+                           "     1\n";
+       assertEquals(expected, IntegerDivision.makeDivision(78945, 4));
+   }
+	
+	@Test
+   void makeDivision_DivisionWithNegativeArgument_True() {
+       String expected =   "_78945|4\n" + 
+                           " 4    |------\n"+
+                           " -    |-19736\n" + 
+                           "_38\n" + 
+                           " 36\n" + 
+                           " --\n"+
+                           " _29\n"+
+                           "  28\n" + 
+                           "  --\n" + 
+                           "  _14\n" + 
+                           "   12\n" + 
+                           "   --\n"+
+                           "   _25\n"+
+                           "    24\n"+
+                           "    --\n"+
+                           "     1\n";
+       assertEquals(expected, IntegerDivision.makeDivision(78945, -4));
+   }
+	
+	@Test
+   void makeDivision_DivisionWithNegativeDivisor_True() {
+       String expected =   "_78945|4\n" + 
+                           " 4    |------\n"+
+                           " -    |-19736\n" + 
+                           "_38\n" + 
+                           " 36\n" + 
+                           " --\n"+
+                           " _29\n"+
+                           "  28\n" + 
+                           "  --\n" + 
+                           "  _14\n" + 
+                           "   12\n" + 
+                           "   --\n"+
+                           "   _25\n"+
+                           "    24\n"+
+                           "    --\n"+
+                           "     1\n";
+       assertEquals(expected, IntegerDivision.makeDivision(78945, -4));
+   }
+	
+	@Test
+   void makeDivision_DivisionWithNegativeDividend_True() {
+       String expected =   "_78945|4\n" + 
+                           " 4    |------\n"+
+                           " -    |-19736\n" + 
+                           "_38\n" + 
+                           " 36\n" + 
+                           " --\n"+
+                           " _29\n"+
+                           "  28\n" + 
+                           "  --\n" + 
+                           "  _14\n" + 
+                           "   12\n" + 
+                           "   --\n"+
+                           "   _25\n"+
+                           "    24\n"+
+                           "    --\n"+
+                           "     1\n";
+       assertEquals(expected, IntegerDivision.makeDivision(-78945, 4));
+   }
+	
+	@Test
+   void makeDivision_NonZeroRremnant_True() {
+       String expected =   "_33|2\n" + 
+                           " 2 |--\n"+
+                           " - |16\n" + 
+                           "_13\n" + 
+                           " 12\n" + 
+                           " --\n"+
+                           "  1\n";
+       assertEquals(expected, IntegerDivision.makeDivision(33, 2));
+   }
+	
+	@Test
+   void makeDivision_DivisionWithZeroArgument_ExceptionThrown() {
+       IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> IntegerDivision.makeDivision(78945, 0),
+ 				"Divisor cannot be 0, division by zero");
+ 		assertEquals("Divisor cannot be 0, division by zero", thrown.getMessage());
+   }
+
+}
