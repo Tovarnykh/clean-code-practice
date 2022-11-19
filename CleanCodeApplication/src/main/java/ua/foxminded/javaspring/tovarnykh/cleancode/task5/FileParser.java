f2095cf25bd1a8d3f12af802f14e793ac99281c0
Viktor Tovarnykh <milaslavska2@gmail.com>
2022-11-19 16:17:50 +0100

    Implement LeaderBoard creator

--- CleanCodeApplication/src/main/java/ua/foxminded/javaspring/tovarnykh/cleancode/task5/FileParser.java
+++ CleanCodeApplication/src/main/java/ua/foxminded/javaspring/tovarnykh/cleancode/task5/FileParser.java
@@ -11,7 +11,8 @@ import java.util.stream.Collectors;
 
 public class FileParser {
 
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS").withZone(ZoneOffset.UTC);
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS")
+	    .withZone(ZoneOffset.UTC);
     private static final String DELIMITER = "_";
 
     private FileParser() {
@@ -20,19 +21,13 @@ public class FileParser {
 
     public static Map<String, Instant> getStatistic(String filepath) throws IOException {
 	return Files.readAllLines(Paths.get(filepath)).stream()
-		.collect(Collectors.toMap(
-			(String line) -> line.substring(0,3),
+		.collect(Collectors.toMap((String line) -> line.substring(0, 3),
 			(String line) -> Instant.from(DATE_TIME_FORMATTER.parse(line.substring(3)))));
     }
 
     public static Map<String, String> getAbbreviations(String filepath) throws IOException {
-	return Files.readAllLines(
-		Paths.get(filepath))
-		.stream().map(line -> line.split(DELIMITER))
-		.collect(
-		Collectors.toMap(
-			(String[] line) -> line[0], 
-			(String[] line) -> line[1] + DELIMITER + line[2]));
+	return Files.readAllLines(Paths.get(filepath)).stream().map(line -> line.split(DELIMITER)).collect(
+		Collectors.toMap((String[] line) -> line[0], (String[] line) -> line[1] + DELIMITER + line[2]));
     }
 
 }
--- CleanCodeApplication/src/main/java/ua/foxminded/javaspring/tovarnykh/cleancode/task5/FormulaOne.java
+++ CleanCodeApplication/src/main/java/ua/foxminded/javaspring/tovarnykh/cleancode/task5/FormulaOne.java
@@ -1,21 +1,43 @@
 package ua.foxminded.javaspring.tovarnykh.cleancode.task5;
 
+import java.io.IOException;
+import java.time.Duration;
 import java.time.Instant;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class FormulaOne {
-    
+
     private FormulaOne() {
-	
+
     }
-    
-    
-    
-    public static Map<String, Instant> getLeaderBoard(Map<String, Instant> racers) {
-	
-	return racers.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+    public static Map<String, String> getLeaderBoard(String start, String end) throws IOException {
+	return createSortedList(FileParser.getStatistic(start), FileParser.getStatistic(end)).entrySet().stream()
+		.collect(Collectors.toMap(Map.Entry::getKey,
+			racerStatistic -> String.format("%02d:%02d.%03d",
+				TimeUnit.MILLISECONDS.toMinutes(racerStatistic.getValue()),
+				TimeUnit.MILLISECONDS.toSeconds(racerStatistic.getValue()) - TimeUnit.MINUTES
+					.toSeconds(TimeUnit.MILLISECONDS.toMinutes(racerStatistic.getValue())),
+				racerStatistic.getValue() - TimeUnit.SECONDS
+					.toMillis(TimeUnit.MILLISECONDS.toSeconds(racerStatistic.getValue()))),
+			(oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
     }
-    
+
+    public static Map<String, Long> createSortedList(Map<String, Instant> startStatistic,
+	    Map<String, Instant> endStatistic) {
+	Map<String, Long> leaderBoard = new LinkedHashMap<>();
+	startStatistic.forEach((key, value) -> {
+	    Duration d = Duration.between(value, endStatistic.get(key));
+	    leaderBoard.put(key, d.toMillis());
+	});
+	return leaderBoard.entrySet().stream().sorted(Map.Entry.comparingByValue()).collect(Collectors
+		.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
 }
